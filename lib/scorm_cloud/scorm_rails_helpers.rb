96f5103aeff2a3c9e014706ed8118240e53dcec2
Ken Richard <kenrichar@gmail.com>
2011-07-09 10:39:41 -0400

    Working on helpers

--- lib/scorm_cloud/scorm_rails_helpers.rb
+++ lib/scorm_cloud/scorm_rails_helpers.rb
@@ -1,25 +1,12 @@
 module ScormCloud
   module ScormRailsHelpers
 
-  	def self.scorm_appid=(appid)
-  		@scorm_appid = appid
-  	end
-
-    def self.scorm_appid
-      @scorm_appid
-    end
-
-  	def self.scorm_secretkey=(secretkey)
-  		@scorm_secretkey = secretkey
-  	end
-
-    def self.scorm_secretkey
-      @scorm_secretkey
-    end
-
   	def scorm_cloud
   		unless @scorm_cloud
-  			@scorm_cloud = ::ScormCloud::ScormCloud.new(self.scorm_appid, self.scorm_secretkey)
+  			@scorm_cloud = ::ScormCloud::ScormCloud.new(
+              Rails.configuration.scorm_appid,
+              Rails.configuration.scorm_secretkey
+            )
   		end
   		@scorm_cloud
   	end
