45a29d5a36384205738c454a830285fbec5f4dbc
Ken Richard <kenrichar@gmail.com>
2011-07-09 10:13:51 -0400

    Cleanup readme; Fix helpers

--- lib/scorm_cloud/scorm_rails_helpers.rb
+++ lib/scorm_cloud/scorm_rails_helpers.rb
@@ -1,17 +1,17 @@
 module ScormCloud
   module ScormRailsHelpers
 
-  	def self.apikey=(apikey)
-  		@scorm_apiky = value
+  	def self.appid=(appid)
+  		@scorm_appid = appid
   	end
 
-  	def self.secret=(secret)
-  		@scorm_secret = value
+  	def self.secretkey=(secretkey)
+  		@scorm_secretkey = secretkey
   	end
 
   	def scorm_cloud
   		unless @scorm_cloud
-  			@scorm_cloud = ScormCloud::ScormCloud.new(@scorm_apikey, @scorm_secret)
+  			@scorm_cloud = ScormCloud::ScormCloud.new(@scorm_appid, @scorm_secretkey)
   		end
   		@scorm_cloud
   	end
