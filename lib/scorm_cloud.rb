b21571339bd06f0a5446e4b59041e8274b891c5c
Ken Richard <kenr@aeseducation.com>
2011-07-12 15:40:46 -0400

    Check for rails 3 before using railtie

--- lib/scorm_cloud.rb
+++ lib/scorm_cloud.rb
@@ -19,7 +19,8 @@ require 'scorm_cloud/reporting_service'
 require 'scorm_cloud/dispatch_service'
 require 'scorm_cloud/export_service'
 
-require 'scorm_cloud/railtie' if defined?(Rails)
+# Rails 3 Integration
+require 'scorm_cloud/railtie' if defined?(Rails::Railtie)
 
 module ScormCloud
 	class ScormCloud < Base
--- lib/scorm_cloud/railtie.rb
+++ lib/scorm_cloud/railtie.rb
@@ -5,9 +5,9 @@ class ScormCloud::Railtie < Rails::Railtie
 
 	config.scorm_cloud = ActiveSupport::OrderedOptions.new
 
-  initializer "scorm_cloud.scorm_rails_helpers" do
-    ActionController::Base.send :include, ScormCloud::ScormRailsHelpers
-  end
+	initializer "scorm_cloud.scorm_rails_helpers" do
+		ActionController::Base.send :include, ScormCloud::ScormRailsHelpers
+	end
 
 end
 
