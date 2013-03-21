7ab154adcad334005ffc389d403e8caa0d16f7a1
Murat GUZEL <guzelmurat@gmail.com>
2013-03-21 17:19:43 +0200

    RegistrationResult class for course registration result

--- lib/scorm_cloud/registration_service.rb
+++ lib/scorm_cloud/registration_service.rb
@@ -33,7 +33,7 @@ module ScormCloud
 		end
 
 		def get_registration_result_for_course(reg_id)
-			xml = REXML::Document.new connection.call("rustici.registration.getRegistrationResult", { :regid => reg_id, :format => "course" })
+			xml = REXML::Document.new connection.call_raw("rustici.registration.getRegistrationResult", { :regid => reg_id, :format => "course" })
 			#xml.element["/rsp"].map { |e| RegistrationResult.from_xml(e) }
 		end
 
